983b21e0a39acccc789a5bd1a277baaf3ae9c08a
Petrut Petrache <cristian.petrut.petrache@sap.com>
2020-05-22 18:30:58 +0300

    Setup sql server

--- src/main/java/com/sap/ibso/hackathon/booker/config/JPAConfig.java
+++ src/main/java/com/sap/ibso/hackathon/booker/config/JPAConfig.java
@@ -1,11 +1,79 @@
 package com.sap.ibso.hackathon.booker.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.sap.ibso.hackathon.booker.jpa.repo")
 @EnableTransactionManagement
 public class JPAConfig {
+
+    @Value("${WFO_BOOKER_JDBC_URL:jdbc:sqlserver://localhost:1433}")
+    private String jdbcUrl;
+
+    @Value("${WFO_BOOKER_DB_USERNAME:username}")
+    private String dbUsername;
+
+    @Value("${WFO_BOOKER_DB_PASSWORD:password)}")
+    private String dbPassword;
+
+    @Value("${WFO_BOOKER_DB_SHOW_SQL:false}")
+    private Boolean showSql;
+
+    @Value("${WFO_BOOKER_DB_HBM_TO_DDL:update}")
+    private String hbm2ddl;
+
+    @Bean
+    public DataSource getDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(jdbcUrl);
+        config.setUsername(dbUsername);
+        config.setPassword(dbPassword);
+        config.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        config.setMinimumIdle(2);
+        config.setMaximumPoolSize(10);
+        config.setConnectionTimeout(30000);
+        config.setIdleTimeout(600000);
+        config.setMaxLifetime(1800000);
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setPersistenceProvider(new HibernatePersistenceProvider());
+        entityManagerFactoryBean.setPackagesToScan("com.sap.ibso.hackathon.booker.jpa.model");
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaPropertyMap(getJPAProperties());
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        return entityManagerFactoryBean;
+    }
+
+    public Map<String, Object> getJPAProperties() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.SQLServerDialect");
+        properties.put("hibernate.show_sql", showSql);
+        properties.put("hibernate.hbm2ddl.auto", hbm2ddl);
+        properties.put("hibernate.jdbc.time_zone", "UTC");
+        return properties;
+    }
 }
